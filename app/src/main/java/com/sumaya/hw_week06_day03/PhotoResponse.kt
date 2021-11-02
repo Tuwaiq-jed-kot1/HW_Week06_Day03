507119d93cd171df02480af727790f8dfcbe580a
Marah <marah.bossi.jed.k1@tuwaiq.edu.sa>
2021-11-03 01:43:45 +0300

    done

---
+++ app/src/main/java/com/sumaya/hw_week06_day03/PhotoResponse.kt
@@ -0,0 +1,9 @@
+package com.sumaya.hw_week06_day03
+
+import com.google.gson.annotations.SerializedName
+
+
+class PhotoResponse {
+    @SerializedName("photo")
+    lateinit var galleryItems: List<GalleryItem>
+}
