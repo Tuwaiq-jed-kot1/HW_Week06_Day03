5b888f0a8027fc02750b866eb730595bc66db14c
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-11-03 07:27:33 +0300

    Done

---
+++ app/src/main/java/com/sumaya/hw_week06_day03/PhotoGalleryViewModel.kt
@@ -0,0 +1,12 @@
+package com.sumaya.hw_week06_day03
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+
+class PhotoGalleryViewModel : ViewModel() {
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchPhotos()
+    }
+}
