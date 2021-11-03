b4b194782380e70e278d75c47d5daf6bc46eda33
HP <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2021-11-03 07:55:49 +0300

    HW!!

---
+++ app/src/main/java/com/sumaya/hw_week06_day03/FlickrApi.kt
@@ -0,0 +1,15 @@
+package com.sumaya.hw_week06_day03
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=1af94995d28f2982c2f46b0217d23524" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
