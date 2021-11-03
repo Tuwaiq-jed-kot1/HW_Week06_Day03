87023223142db8cf607ec5c77186a60751562124
asmaa <Asma.Alghamdi.jed.k1@tuwaiq.edu.sa>
2021-11-03 09:03:39 +0300

    w6-hw3

--- app/src/main/java/com/sumaya/hw_week06_day03/FlickrApi.kt
+++ app/src/main/java/com/sumaya/hw_week06_day03/FlickrApi.kt
@@ -4,12 +4,10 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=yourApiKeyHere" +
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+        "&api_key=7389686c4a2c69250786e34ffb34d48f"+
                 "&format=json" +
                 "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+                "&extras=url_s")
     fun fetchPhotos(): Call<FlickrResponse>
 }
