deb6f618dd628ec5ca875310cc1309e8baf73105
Basil19 <bojsl-9@hotmail.com>
2021-11-03 08:53:02 +0300

    WEEK6-DAY3->DONE..

---
+++ app/src/main/java/com/sumaya/hw_week06_day03/FlickrApi.kt
@@ -0,0 +1,15 @@
+package com.sumaya.hw_week06_day03
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET(
+    "services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=yourApiKeyHere" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s"
+)
+fun fetchPhotos(): Call<FlickrResponse>
+}
